ce2b3f5862660dba2ed23380714441a0d90e99ae
Umaralixan <umaralixtreme@gmail.com>
2025-11-08 19:01:14 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,191 @@
+// 1 masala
+// let a = 10;
+// let b = 8;
+// console.log(a + b + " Yig'indisi")
+// console.log(a - b + " Ayirmasi")
+// console.log(a * b + " Ko'payitmasi");
+// console.log(a / b + " Bo'linmasi")
+
+// 2 masala
+// let a = prompt("sonni kiriting");
+// let b = prompt("sonni kiriting");
+// let kvadrat = a ** 2;
+// let kub = b ** 3;
+// console.log(kvadrat);
+// console.log(kub);
+
+// 3 masala
+// let son = prompt("Sonni kiriting");
+// if (son % 2 == 0) {
+//   console.log("Juft son");
+// } else {
+//   console.log("Toq son");
+// }
+
+// 4 masala
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a > b) {
+//   console.log("a katta");
+// } else if (a < b) {
+//   console.log("b katta");
+// } else {
+//   console.log("ikkalasi ham teng");
+// }
+
+// 5 masala
+// let yosh = prompt("yoshingizni kiriting");
+// if (yosh >= 18) {
+//   console.log("voyiga yitgan");
+// } else {
+//   console.log("voyaga yetmagan");
+// }
+
+// 6 masala
+// let ball = prompt("Balingizni kiriting");
+// if (ball >= 96) {
+//   console.log("Vunderkind");
+// } else if (ball >= 86) {
+//   console.log("A'lo");
+// } else if (ball >= 71) {
+//   console.log("Yaxshi");
+// } else if (ball >= 60) {
+//   console.log("Qoniqarli");
+// } else if (ball >= 21) {
+//   console.log("Qoniqarsiz");
+// } else if (ball >= 0) {
+//   console.log("Juda yomon");
+// }
+
+// 7 masala
+// let son = prompt("Sonni kiritng");
+// if (son > 0) {
+//   console.log("Musbat son");
+// } else if (son < 0) {
+//   console.log("Manfiy son");
+// } else {
+//   console.log("Nol");
+// }
+
+// 8 masala
+// let x = prompt("x ni kiritng");
+// if (x > 5) {
+//   x = x * 2;
+// } else {
+//   x = x / 3;
+// }
+// console.log(x);
+
+// 9 masala
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let yigindi = a + b;
+// if ( yigindi > 10) {
+//   console.log("Katta sonlar");
+// } else if (yigindi < 10) {
+//   console.log("Kichik sonlar");
+// }
+
+// 10 masala
+// let son = prompt("Sonni kiriting");
+// if (son % 3 === 0 && son % 5 === 0) {
+//   console.log("FizzaBuzz");
+// } else if (son % 3 === 0) {
+//   console.log("Fizza");
+// } else if (son % 5 === 0) {
+//   console.log("Buzz");
+// } else {
+//   console.log("Oddiy son");
+// }
+
+// 11 masala
+// let yil = prompt("Yilni kiriting");
+// if (yil % 4 === 0) {
+//   console.log("Kabisa yili");
+// } else {
+//   console.log("Oddiy yil");
+// }
+
+// 12 masala
+// let harorat = prompt("Haroratni kiriring");
+// if (harorat > 30) {
+//   console.log("Issiq");
+// } else if (harorat > 20) {
+//   console.log("Yoqimli");
+// } else if (harorat < 20) {
+//   console.log("Sovuq");
+// }
+
+// 13 masala
+// let javob = confirm("Davom etasizmi");
+// if (javob) {
+//   console.log("Davom etildi");
+// } else {
+//   console.log("Bekor qilindi");
+// }
+
+// 14 masala
+// let ism = prompt("Ismingizni kiriting");
+// if (ism && ism) {
+//   console.log("Salom");
+// } else {
+//   console.log("Ism kiritilmadi");
+// }
+
+// 15 masala
+// let son = prompt("Sonni kiriting");
+// let natija = son / 2;
+// if (natija > 5) {
+//   console.log("Natija katta");
+// } else {
+//   console.log("Natija kichik");
+// }
+
+// 16 masala
+// let parol = prompt("Parolni kiriting");
+// if (parol === "1234") {
+//   console.log("Kirirsh Muvaffaqiyatli");
+// } else {
+//   console.log("Parol xato");
+// }
+
+// 18 masala
+// let a = prompt("a ni kiriting");
+// let b = prompt("b ni kiriting");
+// let yigindi = a + b;
+// if (yigindi % 2 === 0) {
+//   console.log("Juft natija");
+// } else {
+//   console.log("Toq natija");
+// }
+
+// 19 masala
+// let kun = Number(prompt("Hafta kun raqamini kiriting"));
+// if (kun === 1) {
+//   console.log("Dushanba");
+// } else if (kun === 2) {
+//   console.log("Seshanba");
+// } else if (kun === 3) {
+//   console.log("chorshanba");
+// } else if (kun === 4) {
+//   console.log("Payhanba");
+// } else if (kun === 5) {
+//   console.log("Juma");
+// } else if (kun === 6) {
+//   console.log("Shanba");
+// } else if (kun === 7) {
+//   console.log("Yakshanba");
+// } else {
+//   console.log("Xato raqam kiritdingiz 1 dan 7 gacha raqam kiriting");
+// }
+
+// 20 masala
+// let a = Number(prompt("a sonini kiritng"));
+// let b = Number(prompt("b sonini kiritng"));
+// if (a > 0 && b > 0) {
+//   console.log("Ijobiy sonlar");
+// } else if (a < 0 && b < 0) {
+//   console.log("Manfiy sonlar");
+// } else {
+//   console.log("Turlicha belgili sonlar");
+// }
